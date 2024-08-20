904e00cf049288bf543837c14d32373b0aca2d13
nusrat3657 <nusratjahan3657r@gmail.com>
2024-08-21 01:23:38 +0600

    added hosting links

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:5173", 
+            "https://inventoria-8dd39.web.app", 
+            "https://inventoria-8dd39.firebaseapp.com",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 
