fcc1bd87eb9bb4baad63242f960934087d845a17
nusrat3657 <nusratjahan3657r@gmail.com>
2024-08-21 21:34:23 +0600

    updated some line

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.use(
 app.use(express.json());
 
 
-// console.log(process.env.DB_PASS);
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hsfxbe1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
